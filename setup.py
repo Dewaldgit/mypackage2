1dfee44b831be9b89fd79c97725c3855c268f71b
Dewald Jacobs <dewaldmarelisejacobs@gmail.com>
2021-04-23 13:49:53 +0200

    commit v2 where the docstrings were updated

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mypackage2',
-    version='0.1',
+    version='0.2',
     packages=find_packages(exclude=['tests*']),
     license='DJ',
     description='Second example of a python package',
